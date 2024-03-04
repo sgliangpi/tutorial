ca6abb902567293e4ca5ce6c4ef283a913a96009
sglp <760876814@qq.com>
2024-03-04 17:50:17 +0800

    增加切换动画

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,3 +1,6 @@
-(document.querySelector('.hamburger') as HTMLElement).addEventListener('click', () => {
-  (document.querySelector('.nav-links') as HTMLElement).classList.toggle('expanded');
+
+document.addEventListener('astro:page-load', () => {
+  (document.querySelector('.hamburger') as HTMLElement).addEventListener('click', () => {
+    (document.querySelector('.nav-links') as HTMLElement).classList.toggle('expanded');
+  });
 });
